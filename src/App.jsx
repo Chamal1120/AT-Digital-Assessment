0a3d7a9775d44bb42631a21fc957e0707e35c377
Chamal1120 <kenway213beep@gmail.com>
2024-04-09 00:48:36 +0530

    Optimized the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 import logo from './assets/images/Logo.svg';
-import heroImage from './assets/images/Hero.jpg';
-import section01Image from './assets/images/Section01.webp';
-import section02Image from './assets/images/Section02.webp';
+import heroImage from './assets/images/Hero.avif';
+import section01Image from './assets/images/Section01.avif';
+import section02Image from './assets/images/Section02.avif';
 
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import CloseIcon from '@mui/icons-material/Close';
@@ -12,7 +12,6 @@ import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 
 function App() {
     const [isOpen, setIsOpen] = useState(false);
-
     const [isCollapsed1, setIsCollapsed1] = useState(true);
     const [isCollapsed2, setIsCollapsed2] = useState(true);
     const [isCollapsed3, setIsCollapsed3] = useState(true);
@@ -37,8 +36,8 @@ function App() {
         <>
             <nav className="bg-at-purple p-4">
                 <div className="mx-auto flex max-w-screen-2xl items-center justify-between ">
-                    <div className="block pb-3 pl-3 font-bold text-white">
-                        <img className="w-13" src={logo}></img>
+                    <div className="block cursor-pointer pb-3 pl-3 font-bold text-white">
+                        <img className="w-13" src={logo} alt="AT-logo"></img>
                     </div>
                     <button className="block border-white px-2 py-1 text-white md:hidden" onClick={toggleNavbar}>
                         {isOpen ? '' : <MenuRoundedIcon style={{ fontSize: '3rem' }} />}
@@ -52,7 +51,7 @@ function App() {
                         >
                             <CloseIcon fontSize="large" />
                         </button>
-                        <ul className="items-center justify-center gap-[3vw] md:flex">
+                        <ul className="z-40 items-center justify-center gap-[3vw] md:flex">
                             <li className={`mr-4 max-md:pb-10 ${isOpen ? '' : 'hidden'}`}>
                                 <a href="#" className="max-md:text-black  hover:max-md:text-slate-500">
                                     HOME
@@ -83,11 +82,11 @@ function App() {
                 </div>
             </nav>
             <div className="mx-auto max-w-screen-2xl">
-                <section className="lg:max-h-128 overflow-hidden">
-                    <div className="flex h-full w-full items-center justify-center lg:z-30">
+                <section className="lg:relative">
+                    <div className="Lg:z-0 flex h-full w-full items-center justify-center">
                         <img src={heroImage} alt="Hero Image" />
                     </div>
-                    <div className="heroText bg-gradient-to-r from-at-green to-at-blue px-5 pb-8 pt-6 max-md:w-full lg:z-40 lg:max-w-xl lg:-translate-y-64 lg:translate-x-12">
+                    <div className="heroText bg-gradient-to-r from-at-green to-at-blue px-5 pb-8 pt-6 max-md:w-full lg:absolute lg:bottom-[2rem] lg:left-[3rem] lg:z-20 lg:max-w-xl xl:bottom-[2rem] xl:left-[6rem]">
                         <h2 className="mb-5 font-inter text-4xl/9 font-bold tracking-tight text-white">
                             We Crush Your Competitors, Goals, And Sales Records - Without The B.S.
                         </h2>
@@ -145,7 +144,7 @@ function App() {
                     <h2 className="mb-5 text-center font-poppins text-3xl/8 font-semibold tracking-normal text-at-purple">
                         Frequently asked questions
                     </h2>
-                    <div className="bg-at-purple-light mx-auto mb-4 w-full max-w-4xl rounded-lg p-4">
+                    <div className="mx-auto mb-4 w-full max-w-4xl rounded-lg bg-at-purple-light p-4">
                         <button
                             className="flex w-full items-center justify-between rounded-md p-2"
                             onClick={toggleCollapse1}
@@ -165,7 +164,7 @@ function App() {
                         </button>
                         {!isCollapsed1 && (
                             <div className="mt-2">
-                                <p className="text-at-grey font-inter font-normal leading-7">
+                                <p className="font-inter font-normal leading-7 text-at-grey">
                                     Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum.
                                     Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus
                                     sagittis tortor integer.
@@ -173,7 +172,7 @@ function App() {
                             </div>
                         )}
                     </div>
-                    <div className="bg-at-purple-light mx-auto mb-4 w-full max-w-4xl rounded-lg p-4">
+                    <div className="mx-auto mb-4 w-full max-w-4xl rounded-lg bg-at-purple-light p-4">
                         <button
                             className="flex w-full items-center justify-between rounded-md p-2"
                             onClick={toggleCollapse2}
@@ -193,7 +192,7 @@ function App() {
                         </button>
                         {!isCollapsed2 && (
                             <div className="mt-2">
-                                <p className="text-at-grey font-inter font-normal leading-7">
+                                <p className="font-inter font-normal leading-7 text-at-grey">
                                     Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum.
                                     Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus
                                     sagittis tortor integer.
@@ -201,7 +200,7 @@ function App() {
                             </div>
                         )}
                     </div>
-                    <div className="bg-at-purple-light mx-auto mb-4 w-full max-w-4xl rounded-lg p-4">
+                    <div className="mx-auto mb-4 w-full max-w-4xl rounded-lg bg-at-purple-light p-4">
                         <button
                             className="flex w-full items-center justify-between rounded-md p-2"
                             onClick={toggleCollapse3}
@@ -221,7 +220,7 @@ function App() {
                         </button>
                         {!isCollapsed3 && (
                             <div className="mt-2">
-                                <p className="text-at-grey font-inter font-normal leading-7">
+                                <p className="font-inter font-normal leading-7 text-at-grey">
                                     Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum.
                                     Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus
                                     sagittis tortor integer.
@@ -230,7 +229,9 @@ function App() {
                         )}
                     </div>
                 </section>
-                <footer className="bg-at-purple pt-10">
+            </div>
+            <footer className="bg-at-purple pt-10">
+                <div className="mx-auto max-w-screen-2xl">
                     <div className="ml-5 mr-5 justify-center text-white md:ml-8 md:mr-8 lg:flex lg:flex-row">
                         <div className="lg:basis-2/4 xl:basis-3/5">
                             <div>
@@ -247,18 +248,18 @@ function App() {
                             <div className="pb-10 md:mr-28">
                                 <h3 className="mb-2 font-inter text-xl font-semibold">Our Technologies</h3>
                                 <ul className="font-inter text-sm font-normal leading-7">
-                                    <li>ReactJS</li>
-                                    <li>Gatsby</li>
-                                    <li>NextJS</li>
-                                    <li>NodeJS</li>
+                                    <li className="cursor-pointer">ReactJS</li>
+                                    <li className="cursor-pointer">Gatsby</li>
+                                    <li className="cursor-pointer">NextJS</li>
+                                    <li className="cursor-pointer">NodeJS</li>
                                 </ul>
                             </div>
                             <div className="pb-10">
                                 <h3 className="mb-2 font-inter text-xl font-semibold">Our Services</h3>
                                 <ul className="font-inter text-sm font-normal leading-7">
-                                    <li>Social Media Marketing</li>
-                                    <li>Web & Mobile App Development</li>
-                                    <li>Data and Analytics</li>
+                                    <li className="cursor-pointer">Social Media Marketing</li>
+                                    <li className="cursor-pointer">Web & Mobile App Development</li>
+                                    <li className="cursor-pointer">Data and Analytics</li>
                                 </ul>
                             </div>
                         </div>
@@ -266,13 +267,13 @@ function App() {
                     <div className="text-white">
                         <div className="mx-auto mb-2 max-w-xl justify-center border-t border-white"></div>
                         <div className="mx-auto flex flex-row justify-center pb-4 font-inter text-sm font-light leading-5">
-                            <p className="mr-2">Privacy Policy</p>
+                            <p className="mr-2 cursor-pointer">Privacy Policy</p>
                             <p className="mr-2">|</p>
-                            <p>Terms & Conditions</p>
+                            <p className="cursor-pointer">Terms & Conditions</p>
                         </div>
                     </div>
-                </footer>
-            </div>
+                </div>
+            </footer>
         </>
     );
 }
